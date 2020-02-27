5184accf7abe6bbed0ac10bc3d47c13ab5fd995c
Mohammad Rahimi <mashdimamad@gmail.com>
2020-02-26 19:32:46 -0800

    Update naive_bayes_model.py

--- naive_bayes_model.py
+++ naive_bayes_model.py
@@ -1,10 +1,12 @@
+#https://www.aclweb.org/anthology/P12-2018.pdf
+
 x = dt[ids]; y = dtarget[ids]
 x_val = dt[~ids]; y_val = dtarget[~ids]
 
-p = x[y==1].sum(0) + 1
-q = x[y==0].sum(0) + 1
+p = x[y==0].sum(0) + 1
+q = x[y==1].sum(0) + 1
 r = np.log((p/p.sum())/(q/q.sum()))
-b = np.log(len(q)/len(q))
+b = np.log((y==1).mean() / (y==0).mean())
 
 pred = x_val[r.index] @ r.T + b
 print('test score:', metrics.roc_auc_score(y_val, pred))
